4a0a6547d7f95123b750f0d7a073cfff482e9918
RahulPardeshi <rahulpardeshi949@gmail.com>
2026-02-02 17:57:57 +0530

    this is leet code program

--- FCAPP.java
+++ FCAPP.java
@@ -1,25 +1,58 @@
-import java.util.*;
-class A{
+/*
+Input: arr = [4,3,1,1,3,3,2], k = 3
+Output: 2
+Explanation: Remove 4, 2 and either one of the two 1s or three 3s. 1 and 3 will be left.
+ 
+*/
 
-  public static void show()
-  {
-    System.out.println("I am parent show");
-  }
-}
-class B extends A{
-  
-   public static void show()
-   { show();
-     System.out.println("I am child show");
-   }
-}
+import java.util.*;
 public class FCAPP
 {
-   public static void main(String args[])
-   {
-      //A a=new B();
-      // a.show();
-     B.show();
- 
-   }
+  public static void main(String agrs[])
+  {
+      int arr[]={4,3,1,1,3,3,2};
+      int k = 3;
+
+      HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+
+        for(int v:arr)
+        {
+            map.put(v,map.getOrDefault(v,0)+1);
+        }
+      
+       Set<Map.Entry<Integer,Integer>>set=map.entrySet();
+    
+     /*
+      for(Map.Entry<Integer,Integer>m:set)
+     {
+        System.out.println(m.getKey()+"--->"+m.getValue());
+      }
+     */
+   
+      ArrayList<Integer> freq=new ArrayList<>();
+   
+     for(int v:map.values())
+     {
+        freq.add(v);
+     }
+
+     Collections.sort(freq);
+
+    int uniques=freq.size();
+
+    for(int f:freq)
+    {
+     if(k>=f)
+     { 
+       k-=f;
+      uniques--;
+     }
+    else
+     {
+        break;
+     } 
+    }
+
+     System.out.println("unique elements left :"+uniques); 
+  }
 }
