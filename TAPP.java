c1ef43fee7af3456ba595178f44981049ab575a4
RahulPardeshi <rahulpardeshi949@gmail.com>
2026-02-20 21:32:49 +0530

    this is thread application . I am ceating using arrow function

--- TAPP.java
+++ TAPP.java
@@ -1,21 +1,22 @@
 import java.util.*;
-class J extends Thread
-{  public void run()
-   { try{
-          for(int i=0;i<5;i++)
-          { System.out.println("Good Morning ..."+i);
-            Thread.sleep(10000);
-          }
-        }
-     catch(Exception e)
-       { System.out.println("Exception occured"+e); 
-       }
-   }
-}
 public class TAPP
-{ public static void main(String args[])
-  {  J th=new J();
-      th.start();
-
-  }//main
-}//class
+{
+  public static void main(String args[])
+  {
+    Thread th=new Thread(()->{
+    
+     try{
+     
+          for(int i=0;i<5;i++)
+          {
+            System.out.println(i);
+          }
+       }
+     catch(Exception e)
+     {
+       System.out.println(e);
+     }
+  });
+  th.start();
+  }
+}
